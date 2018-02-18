b7b4d1fbf53150d97e27cd410e57c22dd4f5507c
warpje <admin@warpsoft.xyz>
2018-02-18 17:15:24 +0100

    Base Finish!

---
+++ NaLP - Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NaLP___Server
+{
+  internal class Program
+  {
+    [STAThread]
+    public static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new frmMain());
+    }
+  }
+}
